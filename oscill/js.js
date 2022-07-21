7dd48235cff5e857c68730d2707ddfa7b27ab951
Flagrantior <flagrantior@gmail.com>
2022-07-21 18:41:27 +0300

    Oscill playing live sound.

--- oscill/js.js
+++ oscill/js.js
@@ -7,8 +7,7 @@ let ampl = 0.5;
 let speed = 0.1;
 let sphTail = 1000;
 let sphDetail = 1;
-let freqes = [[], []];
-let r_min = 0; let r_max = 1000;
+let r_min = 1; let r_max = 1000;
 let offLis = 0;
 
 function draw() {
@@ -17,27 +16,44 @@ function draw() {
 	timer+=speed;
 } let timerId = setInterval(draw, 10);
 
+let ctx = new AudioContext();
+let oscs = [];
+let gains = [];
+let osc_on = false;
+
 function addOsc() {
-	freqes[0].push(0);
-	freqes[1].push(1);
+	let osc = ctx.createOscillator();
+	let gain = ctx.createGain();
+	osc.frequency.value = 440;
+	gain.gain.value = 1;
+	osc.connect(gain).connect(ctx.destination);
+	osc.type = 'sine';
+	oscs.push(osc);
+	gains.push(gain);
+	osc.start();
+	osc.disconnect();
 	document.querySelector('#inputHz').innerHTML = '';
-	for (let i=0; i<freqes[0].length; i++) {
-		document.querySelector('#inputHz').innerHTML += 
+	for (let i=0; i<oscs.length; i++) {
+		document.querySelector('#inputHz').innerHTML +=
 		`<div class="oscInputs" id="osc_${i}">
-			<input value="${freqes[0][i]}" class="oscHz" type="range" step="0.01" min="${r_min}" max="${r_max}" oninput="freqes[0][${i}]=eval(this.value); this.nextSibling.nextSibling.firstChild.value=this.value">
-			[<form onsubmit="this.parentNode.querySelector('input').value=eval(this.firstChild.value); freqes[0][${i}]=eval(this.firstChild.value); return false;"><input value="${freqes[0][i]}" type="text" size="5"></form>]
-			<input value="${freqes[1][i]}" class="oscVol" type="range" step="0.01" min="0" max="2" oninput="freqes[1][${i}]=Number(this.value);">
-			<button onclick="delOsc(${i})">X</button>
+			<input value="${oscs[i].frequency.value}" class="oscHz" type="range" step="0.01" min="${r_min}" max="${r_max}" oninput="oscs[${i}].frequency.value=eval(this.value); this.nextSibling.nextSibling.firstChild.value=this.value">
+			[<form onsubmit="this.parentNode.querySelector('input').value=eval(this.firstChild.value); oscs[${i}].frequency.value=eval(this.firstChild.value); return false;"><input value="${oscs[i].frequency.value}" type="text" size="5"></form>]
+			<input value="${gains[i].gain.value}" class="oscVol" type="range" step="0.01" min="0" max="2" oninput="gains[${i}].gain.value=Number(this.value);">
+			<button onclick="delOsc(${i})">❌</button>
 		</div>`;
 	}
+
+	playTones(); playTones();
 } addOsc()
+
 document.querySelector('.oscHz').value = 440;
-freqes[0][0]=eval(440);
 document.querySelector('#osc_0 form input').value = 440;
 
 function delOsc(id) {
-	freqes[0].splice(id, 1);
-	freqes[1].splice(id, 1);
+	gains[id].disconnect();
+	oscs[id].disconnect();
+	oscs.splice(id, 1);
+	gains.splice(id, 1);
 	document.querySelector(`#osc_${id}`).remove();
 }
 
@@ -54,9 +70,9 @@ function drawLis() {
 	for (let x=0; x<=sphTail; x=x+(sphDetail)) {
 		currentY = 0;
 		currentX = 0;
-		for (let i=0; i<freqes[0].length; i++) {
-			currentY += Math.sin(freqes[0][i]/ctxLis.canvas.height * (x*zoom+timer))*freqes[1][i];
-			currentX += Math.cos(freqes[0][i]/ctxLis.canvas.height * (x*zoom+timer+offLis))*freqes[1][i];
+		for (let i=0; i<oscs.length; i++) {
+			currentY += Math.sin(oscs[i].frequency.value/ctxLis.canvas.height * (x*zoom+timer))*gains[i].gain.value;
+			currentX += Math.cos(oscs[i].frequency.value/ctxLis.canvas.height * (x*zoom+timer+offLis))*gains[i].gain.value;
 		}
 		currentX = currentX*ampl;
 		currentY = currentY*ampl;
@@ -74,8 +90,8 @@ function drawOsc() {
 
 	for (let x=0; x<=ctxOsc.canvas.width; x++) {
 		currentY = 0;
-		for (let i=0; i<freqes[0].length; i++) {
-			currentY += (Math.sin(freqes[0][i]/ctxOsc.canvas.width * (x*zoom+timer)) * freqes[1][i]);
+		for (let i=0; i<oscs.length; i++) {
+			currentY += (Math.sin(oscs[i].frequency.value/ctxOsc.canvas.width * (x*zoom+timer)) * gains[i].gain.value);
 		}
 		currentY = currentY*ampl;
 		ctxOsc.lineTo(x, ctxOsc.canvas.height/2 + ctxOsc.canvas.height/4 * (currentY));
@@ -94,27 +110,12 @@ document.querySelector('#rr_detail').value = 2;
 document.querySelector('#rr_tail').value = 1000;
 document.querySelector('#rr_offLis').value = 0;
 
-function playTone(freq, gain) {
-    let context = new AudioContext();
-    let volume = context.createGain();
-    volume.connect(context.destination);
-    volume.gain.value = gain;
-    osc = context.createOscillator();
-    osc.type = 'sine';
-    osc.frequency.value = freq;
-    osc.connect(volume);
-    osc.start();
-    console.log('Playing at frequency ' + freq + ' with volume ' + gain);
-	osc.stop(context.currentTime + 3)
-}
-
 function playTones() {
-	for (let i=0; i<freqes[0].length; i++) {
-		playTone(freqes[0][i], freqes[1][i]*ampl);
-	}
+	osc_on? oscs.map(osc => osc.disconnect()) : oscs.map((osc,i) => osc.connect(gains[i]).connect(ctx.destination));
+	osc_on = !osc_on;
 }
 
 function rerangeOsc() {
-	document.querySelectorAll('.oscHz').forEach((e, i) => {e.min=r_min; e.max=r_max; e.value=freqes[0][i]});
+	document.querySelectorAll('.oscHz').forEach((e, i) => {e.min=r_min; e.max=r_max; e.value=osos[i].frequency.value});
 }
 
